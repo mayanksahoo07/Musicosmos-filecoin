04ccb8b1e7dc90d420f236f915f0162cb9bcd92a
mayanksahoo07 <107427678+mayanksahoo07@users.noreply.github.com>
2022-09-21 21:49:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -130,7 +130,7 @@ function App() {
             window.web3 = new Web3(window.web3.currentProvider);
             await addPolygonTestnetNetwork();
         } else {
-            // const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+            const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
             window.web3 = new Web3(new Web3.providers.HttpProvider("https://polygon-mumbai.infura.io/v3/6f89b4b5242a4191af04c7939d66d6e8"));
             window.alert(
                 "Non-Ethereum browser detected. You cannot perform any transactions on the blockchain, however you will still be able to watch all content present on the blockchain. To make transactions you should consider installing Metamask"
@@ -141,14 +141,14 @@ function App() {
 
         // This results in a bug which converts all uppercase alphabets in the address to lowercase
         // accounts = await window.ethereum.request({ method: "eth_accounts" });
-        accounts = await web3.current.eth.getAccounts();
-        // accounts = await ethereum.request({ method: "eth_requestAccounts" });
+        
+        accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
         // Load account
         setAccount(accounts[0]);
 
-        
-        const networkData = Musicosmos.networks[ENV.BLOCKCHAIN_NETWORK_ID];
+        const networkId = await web3.eth.net.getid()
+        const networkData = Musicosmos.networks[networkId];
         if (networkData) {
             const _musicosmos = new web3.current.eth.Contract(Musicosmos.abi, networkData.address);
             setMusicosmos(_musicosmos);
